921986235d30efd61052038a26f382340768fb7e
Alfredo <puertorocat@gmail.com>
2021-02-16 22:44:12 -0500

    Register DB Tables
    The tables for the basic register system.

---
+++ ARMData/dbo/Tables/SaleDetail.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [SaleId] INT NOT NULL, 
+    [ProdcutId] INT NOT NULL,
+    [Quantity] INT NOT NULL DEFAULT 1,
+    [PurchasePrice] MONEY NOT NULL,  
+    [Tax] NCHAR(10) NOT NULL DEFAULT 0, 
+   
+)
